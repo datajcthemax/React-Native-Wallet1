a950dd0ed82485cfeef37bb5c21eae4b2754d359
jai <data.jcthemax@gmail.com>
2025-06-27 09:59:57 +0900

    first commit

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import dotenv from "dotenv";
+import { sql } from "./config/db.js";
+
+dotenv.config();
+
+const app = express()
+
+const PORT = process.env.PORT || 5001;
+
+async function initDB() {
+    try {
+        await sql `CREATE TABLE transactions(
+            id SERIAL PRIMARY KEY,
+            user_id VARCHAR(255) NOT NULL,
+            title VARCHAR(255) NOT NULL,
+            amount DECIMAL(10,2) NOT NULL,
+            category VARCHAR(255) NOT NULL,
+            created_at DATE NOT NULL DEFAULT CURRENT_DATE
+        )`
+
+        console.log("database initialized sucessfully")
+    } catch (error) {
+        console.log("Error initiolizing DB",error)
+        process.exit(1) // status code 1 means failure, 0 success
+    }
+}
+
+
+app.get("/", (req, res) => {
+    res.send("It's working");
+});
+
+console.log("my port:", process.env.PORT)
+
+initDB().then(() => {
+    app.listen(PORT,() => {
+        console.log("Server is up and running on PORT:", PORT)
+    });
+})
